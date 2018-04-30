511496f8b77be46d101fe67d90a3ae57643b471d
RohitAbrahamPaul <38557253+RohitAbrahamPaul@users.noreply.github.com>
2018-04-30 19:46:53 +0200

    Added i, j int declarration

--- boundary_val.c
+++ boundary_val.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 void boundaryvalues(int imax,int jmax,double** U,double** V){
-    for (j=1;j<imax;j++){
+    for (int j=1;j<imax;j++){
         U[0][j] =0;
         U[imax][j]=0;
         V[0][j] = -V[1][j];
         V[imax+1][j] = -V[imax][j];
         
     }
-    for (i=1;i<imax;i++){
+    for (int i=1;i<imax;i++){
         V[i][0]=0;
         V[i][jmax]=0;
         U[i][0]=-U[i][1];
