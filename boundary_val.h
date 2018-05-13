142a694dc70d1fca4d432c553c31e41d5a8012b6
Madwaraj <38559186+Madwaraj@users.noreply.github.com>
2018-05-13 11:09:57 +0200

    Add files via upload

--- boundary_val.h
+++ boundary_val.h
@@ -5,11 +5,31 @@
 /**
  * The boundary values of the problem are set.
  */
+
+void special_boundary(int imax,int jmax,double **U,double **V,int **flag);
+
 void boundaryvalues(
   int imax,
   int jmax,
   double **U,
-  double **V
+  double **V,
+  int **flag
 );
+
+int B_N(int flag);
+
+int B_S(int flag);
+
+int B_O(int flag);
+
+int B_W(int flag);
+
+int B_NO(int flag);
+
+int B_NW(int flag);
+
+int B_SO(int flag);
+
+int B_SW(int flag);
 
 #endif
