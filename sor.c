6d29778d1703c5b17909549332192dd8cf8b9b2e
Madwaraj <38559186+Madwaraj@users.noreply.github.com>
2018-05-13 20:23:40 +0200

    Add files via upload

--- sor.c
+++ sor.c
@@ -1,4 +1,6 @@
 #include "sor.h"
+#include "init.h"
+#include "boundary_val.h"
 #include <math.h>
 
 void sor(
@@ -9,12 +11,38 @@ void sor(
   int    jmax,
   double **P,
   double **RS,
-  double *res
+  double *res,
+  int **flag
 ) {
   int i,j;
   double rloc;
   double coeff = omg/(2.0*(1.0/(dx*dx)+1.0/(dy*dy)));
-
+    
+    for(int i = 0; i<imax; ++i)
+    {
+        for(int j = 0; j<jmax; ++j)
+        {
+            if ( B_O(flag[i][j]) )  P[i][j] = P[i+1][j];
+            
+            if ( B_W(flag[i][j]) )  P[i][j] = P[i-1][j];
+            
+            if ( B_N(flag[i][j]) )  P[i][j] = P[i][j+1];
+            
+            if ( B_S(flag[i][j]) )  P[i][j] = P[i][j-1];
+            
+            if ( B_NO(flag[i][j]) ) P[i][j] = (P[i][j+1] + P[i+1][j])/2;
+            
+            if ( B_NW(flag[i][j]) ) P[i][j] = (P[i][j+1] + P[i-1][j])/2;
+            
+            if ( B_SO(flag[i][j]) ) P[i][j] = (P[i][j-1] + P[i+1][j])/2;
+            
+            if ( B_SW(flag[i][j]) ) P[i][j] = (P[i][j-1] + P[i-1][j])/2;
+            
+            if (flag[i][j]&(1<<3) ) P[i][j] = 0;
+            
+            if (flag[i][j]&(1<<4) ) P[i][j] = P[i+1][j];
+        }
+    }
   /* SOR iteration */
   for(i = 1; i <= imax; i++) {
     for(j = 1; j<=jmax; j++) {
