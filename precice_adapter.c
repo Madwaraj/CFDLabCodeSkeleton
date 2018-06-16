a488bf0f8bad15b14b1d8a6a694cb33e0bb47edf
Madwaraj <38559186+Madwaraj@users.noreply.github.com>
2018-06-16 13:48:46 +0200

    Add files via upload

--- precice_adapter.c
+++ precice_adapter.c
@@ -2,30 +2,31 @@
 #include "boundary_val.c"
 
 
-int *precice_set_interface_vertices(int imax, int jmax, double dx, double dy, double x_origin, double y_origin, int num_coupling_cells, int meshID, int **FLAG)
+int *precice_set_interface_vertices(int imax, int jmax, double dx, double dy, double x_origin, double y_origin, int num_coupling_cells, int meshID, int **flag)
 {
     int dimension =3;
+    int coupledcell=0;
     int* vertexIDs  = (int*)malloc(num_coupling_cells*sizeof(int));
     double* vertices = (double*) malloc(sizeof(double) * num_coupling_cells * dimension);
     
     for (int j=1; j<=jmax; j++){
-        if(flag[i][j]&1<<9 && flag[i][j]&1<<8){      //East boundary
-        vertices[dimension*coupledcell]     = 0;
+        if(flag[imax+1][j]&1<<9 && flag[imax+1][j]&1<<8){      //East boundary
+        vertices[dimension*coupledcell]     = x_origin + (imax - 1)*dx;
         vertices[dimension*coupledcell + 1] = y_origin + (j - 0.5)*dy;
         vertices[dimension*coupledcell + 2] = 0;
         coupledcell++;
     }
     }
     for (int j=1; j<=jmax; j++){
-        if(flag[i][j]&1<<9 && flag[i][j]&1<<7){     // West boundary
-            vertices[dimension*coupledcell]     = x_origin + (imax - 1)*dx;
+        if(flag[0][j]&1<<9 && flag[0][j]&1<<7){     // West boundary
+            vertices[dimension*coupledcell]     = 0;
             vertices[dimension*coupledcell + 1] = y_origin + (j - 0.5)*dy;
         vertices[dimension*coupledcell + 2] = 0;
         coupledcell++;
     }
     }
     for (int i=1; i<=imax; i++){
-        if(flag[i][j]&1<<9 && flag[i][j]&1<<6){    // Bottom boundary
+        if(flag[i][0]&1<<9 && flag[i][0]&1<<6){    // Bottom boundary
         vertices[dimension*coupledcell] = x_origin + (i - 0.5)*dx;
             vertices[dimension*coupledcell + 1]     = 0;
         vertices[dimension*coupledcell + 2] = 0;
@@ -33,7 +34,7 @@ int *precice_set_interface_vertices(int imax, int jmax, double dx, double dy, do
     }
     }
     for (int i=1; i<=imax; i++){
-        if(flag[i][j]&1<<9 && flag[i][j]&1<<5){   // Top boundary
+        if(flag[i][jmax+1]&1<<9 && flag[i][jmax+1]&1<<5){   // Top boundary
             vertices[dimension*coupledcell] = x_origin + (i - 0.5)*dx;
         vertices[dimension*coupledcell + 1]     = y_origin + (jmax-1)*dy;
         vertices[dimension*coupledcell + 2] = 0;
@@ -47,24 +48,25 @@ int *precice_set_interface_vertices(int imax, int jmax, double dx, double dy, do
 
 void precice_write_temperature(int imax, int jmax, int num_coupling_cells, double *temperature, int *vertexIDs, int temperatureID, double **T, int **flag)
 {
+    int count=0;
     for (int i=1;i<=imax;i++)
     {
-            if(flag[i][j]&1<<9 && flag[i][j]&1<<5){
+            if(flag[i][jmax+1]&1<<9 && flag[i][jmax+1]&1<<5){
              temperature[count]=T[i][jmax];
                 count++;
             }
-            if(flag[i][j]&1<<9 && flag[i][j]&1<<6){
-                temperature[count]=T[i][0];
+            if(flag[i][0]&1<<9 && flag[i][0]&1<<6){
+                temperature[count]=T[i][1];
                 count++;
         }
 }
         for (int j=1; j<=jmax; j++) {
-        if(flag[i][j]&1<<9 && flag[i][j]&1<<8){
+        if(flag[imax+1][j]&1<<9 && flag[imax+1][j]&1<<8){
                 temperature[count]=T[imax][j];
             count++;
             }
-            if(flag[i][j]&1<<9 && flag[i][j]&1<<7){
-                temperature[count]=T[0][j];
+            if(flag[0][j]&1<<9 && flag[0][j]&1<<7){
+                temperature[count]=T[1][j];
                 count++;
             }
         }
